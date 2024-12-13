dfd4df51ccc02c0de907226f1547c3b6a2b112a6
Hammam <h.abutaima@student.aaup.edu>
2024-12-13 15:25:29 +0200

    Build the Header,Skeleton,TodoInput,TodoList components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import styles from './Header.module.css';
-
+import React from 'react'
+import styles from './Header.module.css'
 
 export default function Header({ title }) {
   return (
-    <header className={styles.header}>
-      <p className={styles.headerTitle}>{title}</p>
+    <header>
+        <p className={styles.headerTitle}>{title}</p>
     </header>
-  );
+  )
 }
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,29 +1,121 @@
-import styles from "./TodoList.module.css";
-import Header from "../Header/Header";
+import React, { useContext, useState, useEffect } from 'react';
+import styles from './TodoList.module.css';
+import Header from '../Header/Header';
+import Task from '../Task/Task';
+import Modal from '../Modal/Modal';
+import InputSkeleton from '../InputSkeleton/InputSkeleton';
+import { TaskContext } from '../../store/TaskContext';
 
-const TodoList = () => {
-  const tasks = [];
+export default function TodoList() {
+  const {
+    tasks,
+    toggleTaskDone,
+    renameTask,
+    deleteTask,
+    deleteDoneTasks,
+    deleteAllTasks,
+  } = useContext(TaskContext);
+
+  const [filter, setFilter] = useState('All');
+  const [isDeleteDoneModalOpen, setIsDeleteDoneModalOpen] = useState(false);
+  const [isDeleteAllModalOpen, setIsDeleteAllModalOpen] = useState(false);
+  const [Loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const filteredTasks = tasks.filter((task) => {
+    if (filter === 'Done') return task.isDone;
+    if (filter === 'Todo') return !task.isDone;
+    return true;
+  });
+
+  const openDeleteDoneModal = () => setIsDeleteDoneModalOpen(true);
+  const closeDeleteDoneModal = () => setIsDeleteDoneModalOpen(false);
+
+  const openDeleteAllModal = () => setIsDeleteAllModalOpen(true);
+  const closeDeleteAllModal = () => setIsDeleteAllModalOpen(false);
 
   return (
-    <section className={styles.todoList}>
+    <section className="todo-list">
       <Header title="Todo List" />
-      <div className={styles.taskContainer}>
-        {tasks.map((task) => (
-          <div key={task.id} className={styles.task}>
-            <span className={task.done ? styles.taskDone : ""}>
-              {task.title}
-            </span>
-            <button>Toggle</button>
-            <button>Delete</button>
-          </div>
-        ))}
+      <div className={styles.listActionBtns}>
+        <button
+          onClick={() => setFilter('All')}
+          className={`${styles.filterBtn} sharedFilterBtn shared-hover`}
+        >
+          All
+        </button>
+        <button
+          onClick={() => setFilter('Done')}
+          className={`${styles.filterBtn} sharedFilterBtn shared-hover`}
+        >
+          Done
+        </button>
+        <button
+          onClick={() => setFilter('Todo')}
+          className={`${styles.filterBtn} sharedFilterBtn shared-hover`}
+        >
+          Todo
+        </button>
       </div>
-      <div className={styles.listActions}>
-        <button className={styles.deleteTasks}>Delete All Tasks</button>
-        <button className={styles.deleteTasks}>Delete Done Tasks</button>
+      <div className={styles.todoListContainer}>
+        {Loading ? (
+          <InputSkeleton />
+        ) : filteredTasks.length > 0 ? (
+          filteredTasks.map((task) => (
+            <Task
+              key={task.id}
+              task={task}
+              onToggle={() => toggleTaskDone(task.id)}
+              onRename={renameTask}
+              onDelete={deleteTask}
+            />
+          ))
+        ) : (
+          <p>No tasks match the filter!</p>
+        )}
       </div>
+      <div className={styles.listActionBtns}>
+        <button
+          className={`${styles.deleteBtn} sharedDeleteBtn ${tasks.filter((task) => task.isDone).length === 0 ? 'disabled': ''}`}
+          onClick={openDeleteDoneModal}
+        >
+          Delete Done Tasks
+        </button>
+        <button
+          className={`${styles.deleteBtn} sharedDeleteBtn ${tasks.length === 0 ? 'disabled': ''}`}
+          onClick={openDeleteAllModal}
+        >
+          Delete All Tasks
+        </button>
+      </div>
+
+      {isDeleteDoneModalOpen && (
+        <Modal
+          type="deleteDoneModal"
+          onSave={() => {
+            deleteDoneTasks();
+            closeDeleteDoneModal();
+          }}
+          onClose={closeDeleteDoneModal}
+        />
+      )}
+
+      {isDeleteAllModalOpen && (
+        <Modal
+          type="deleteAllModal"
+          onSave={() => {
+            deleteAllTasks();
+            closeDeleteAllModal();
+          }}
+          onClose={closeDeleteAllModal}
+        />
+      )}
     </section>
   );
-};
-
-export default TodoList;
+}
